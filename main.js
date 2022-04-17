406c405ec7fa6f1b1c06c0887ea7ca5e6b6bb425
Stdemianaa <89221298+Stdemianaa@users.noreply.github.com>
2022-04-17 19:58:02 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+// window.alert("Hello from js file");
+document.write("<h1>hello my name is tony mikhael");
+console.log("hello %c there","color:red;font-size:40px;")
+console.log(typeof "tony mikhael")
